9bdf0c8af99008bf8d677d34e837b828fe7404c5
coolmaark <104413900+coolmaark@users.noreply.github.com>
2022-10-01 14:06:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const ejs = require("ejs");
 const _=require("lodash");
 
 const app = express();
-
+app.use( express.static('images'))
 app.set('view engine', 'ejs');
 //body passer
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 //code starts
 app.get("/",function(req,res){
-  res.render("home")});
-  
+  res.render("home");
+}); 
 //code ends
 app.listen(3000, function() {
   console.log("Server started on port 3000");
