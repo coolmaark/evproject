2222a0fa9f3b734bf098680d875e8f703044f897
rohankarthik090 <111245400+rohankarthik090@users.noreply.github.com>
2023-01-23 13:19:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+
 const express = require("express");
+
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _=require("lodash");
 const { allowedNodeEnvironmentFlags } = require("process");
+
+
 
 const app = express();
 app.use( express.static('public'))
@@ -10,6 +14,10 @@ app.set('view engine', 'ejs');
 //body passer
 app.use(bodyParser.urlencoded({extended: true}));
 //code starts
+
+
+
+
 app.get("/",function(req,res){
   res.render("home");
 }); 
@@ -17,15 +25,22 @@ app.get("/contactus",function(req,res){
   res.render("contactus");
   
   }); 
-app.get("/login",function(req,res){
-  res.render("login");
-}); 
+ 
 app.get("/book",function(req,res){
   res.render("book");
 }); 
 app.get("/signup",function(req,res){
   res.render("signup");
 }); 
+app.get("/hy", function(req,res){
+  res.render("hy");
+});
+
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+ 
 //code ends
 app.listen(3000, function() {
   console.log("Server started on port 3000");
